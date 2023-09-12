f453cb17da3f34c76be11f3f3608ea73313b5a50
SoldierMedic <pv2rojas@gmail.com>
2023-09-11 22:09:21 -0400

    before outlets

---
+++ lab-tunley/TrackCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrackCell.swift
+//  lab-tunley
+//
+//  Created by Marco Rojas on 9/11/23.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
